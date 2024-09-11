fac60a075867b961a475b3672728bfc19ec4a740
Dew25 <dew2@yandex.ru>
2024-09-11 11:19:33 +0300

    Инициация репозитория

---
+++ src/main/java/ee/ivkhkdev/App.java
@@ -0,0 +1,29 @@
+package ee.ivkhkdev;
+
+public class App {
+    public void run(){
+        Address address = new Address();
+        address.setCity("Йыхви");
+        address.setState("Ида-Вирумаа");
+        address.setZip("41536");
+        address.setStreet("Нарвское шоссе");
+        address.setHouse("80");
+        address.setRoom("20");
+
+        Person person = new Person("Ivan","Ivanov", address,10,10,2000);
+        Employee employee = new Employee(person, "Директор", "3000");
+        System.out.printf("Наш работник%nИмя: %s,%n Фамилия %s,%n должность: %s,%n возраст: %d,%n зарплата: %s.%n Проживает по адресу: %s, %s, %s, %s - %s",
+                employee.getPerson().getName(),
+                employee.getPerson().getSurname(),
+                employee.getAppointment(),
+                employee.getPerson().getAge(),
+                employee.getSalary(),
+                employee.getPerson().getAddress().getState(),
+                employee.getPerson().getAddress().getCity(),
+                employee.getPerson().getAddress().getStreet(),
+                employee.getPerson().getAddress().getHouse(),
+                employee.getPerson().getAddress().getRoom()
+        );
+
+    }
+}
